70be27f6d02342920cbea5ce02d63336e41a6f4e
Sneaken <924393527@qq.com>
2022-05-08 17:35:14 +0800

    feat: 通过 wifi 连接 手机

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,12 @@ export const baseConfig = {
   capabilities: {
     platformName: 'Android',
     platformVersion: '10',
-    deviceName: '6be888d9',
+    deviceName: '192.168.0.101:5555',
     automationName: 'UiAutomator2',
+    // 由于手机上已经安装过相关驱动， 所以配置跳过
+    skipDeviceInitialization: true,
+    skipServerInstallation: true,
+    // 一定要设置 不然应用就会被重置
     noReset: true,
     fullReset: false,
   },
--- src/script/adb.md
+++ src/script/adb.md
@@ -10,3 +10,10 @@ dumpsys window | grep mCurrentFocus
 > 持续监听
 
 adb shell logcat
+
+## 3. wifi 连接 手机
+
+1. 首先确保手机连接到电脑上(adb devices 里面有), 电脑和手机连的同一个网络（同一个ip）
+2. 执行 adb tcpip [PORT(自定义)]
+3. adb connect [IP]:[PORT]
+4. 断开手机的数据线（此时 adb devices 里面 只会有 用 wifi 连接的那个设备）
